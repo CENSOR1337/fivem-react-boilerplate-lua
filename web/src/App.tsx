91802c8efedbb9dd6ab66c14c3d46d1c37c042e9
CENSOR_1337 <viixxivvimm@gmail.com>
2023-04-28 22:50:04 +0700

    format: reformat all files in src

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,61 +6,61 @@ import { fetchNui } from "./utils/fetchNui";
 // This will set the NUI to visible if we are
 // developing in browser
 debugData([
-  {
-    action: 'setVisible',
-    data: true,
-  }
+    {
+        action: 'setVisible',
+        data: true,
+    }
 ])
 
 interface ReturnClientDataCompProps {
-  data: any
+    data: any
 }
 
 const ReturnClientDataComp: React.FC<ReturnClientDataCompProps> = ({ data }) => (
-  <>
-    <h5>Returned Data:</h5>
-    <pre>
-      <code>
-        {JSON.stringify(data, null)}
-      </code>
-    </pre>
-  </>
+    <>
+        <h5>Returned Data:</h5>
+        <pre>
+            <code>
+                {JSON.stringify(data, null)}
+            </code>
+        </pre>
+    </>
 )
 
 interface ReturnData {
-  x: number;
-  y: number;
-  z: number;
+    x: number;
+    y: number;
+    z: number;
 }
 
 const App: React.FC = () => {
-  const [clientData, setClientData] = useState<ReturnData | null>(null)
+    const [clientData, setClientData] = useState<ReturnData | null>(null)
 
-  const handleGetClientData = () => {
-    fetchNui<ReturnData>('getClientData').then(retData => {
-      console.log('Got return data from client scripts:')
-      console.dir(retData)
-      setClientData(retData)
-    }).catch(e => {
-      console.error('Setting mock data due to error', e)
-      setClientData({ x: 500, y: 300, z: 200 })
-    })
-  }
+    const handleGetClientData = () => {
+        fetchNui<ReturnData>('getClientData').then(retData => {
+            console.log('Got return data from client scripts:')
+            console.dir(retData)
+            setClientData(retData)
+        }).catch(e => {
+            console.error('Setting mock data due to error', e)
+            setClientData({ x: 500, y: 300, z: 200 })
+        })
+    }
 
-  return (
-    <div className="w-full h-full flex justify-center items-center">
-      <div className="popup-thing">
-        <div>
-          <h1>This is the NUI Popup!</h1>
-          <p>Exit with the escape key</p>
-          <button onClick={handleGetClientData} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Get Client Data
-          </button>
-          {clientData && <ReturnClientDataComp data={clientData} />}
+    return (
+        <div className="w-full h-full flex justify-center items-center">
+            <div className="popup-thing">
+                <div>
+                    <h1>This is the NUI Popup!</h1>
+                    <p>Exit with the escape key</p>
+                    <button onClick={handleGetClientData} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Get Client Data
+                    </button>
+                    {clientData && <ReturnClientDataComp data={clientData} />}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
